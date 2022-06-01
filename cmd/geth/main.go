97f38dad39aaf5a634fc2808e3cfaba39b582f42
Vinod Damle <vinod.damle@kaleido.io>
2022-06-01 08:27:52 -0400

    Allow file descriptor limit to be set via CLI
    * Port of https://github.com/ethereum/go-ethereum/pull/24477, which will
      be picked up in a future release of Quorum.
    * We used to run with a different CLI option `--dbhandles`, I have
      retired that and used the same name as used in the upstream repo now
      that there is a CLI option for the same.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -115,6 +115,7 @@ var (
 		utils.CacheSnapshotFlag,
 		utils.CacheNoPrefetchFlag,
 		utils.CachePreimagesFlag,
+		utils.FDLimitFlag,
 		utils.ListenPortFlag,
 		utils.MaxPeersFlag,
 		utils.MaxPendingPeersFlag,
